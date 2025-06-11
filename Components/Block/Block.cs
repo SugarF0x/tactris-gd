b282da73a8389e13d41a89cf781062e698e5d08c
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-06-11 19:13:29 +0200

    add block expand and collapse animations

--- Components/Block/Block.cs
+++ Components/Block/Block.cs
@@ -58,6 +58,7 @@ public partial class Block : Node2D
     }
 
     private bool _isTweenRunning;
+    
     public bool Move(Vector2 translation, Action callback = null)
     {
         if (_isTweenRunning) return false;
@@ -95,6 +96,30 @@ public partial class Block : Node2D
         return true;
     }
 
+    private bool AnimateScale(Vector2 scale, Action callback = null)
+    {
+        if (_isTweenRunning) return false;
+        _isTweenRunning = true;
+        
+        var tween = CreateTween();
+        tween.SetTrans(Tween.TransitionType.Cubic);
+        tween.SetEase(Tween.EaseType.InOut);
+        tween.Finished += () =>
+        {
+            _isTweenRunning = false;
+            callback?.Invoke();
+        };
+        
+        tween.TweenProperty(this, "scale", scale, .1f);
+
+        return true;
+    }
+
+    public bool Expand(Action callback = null) => AnimateScale(Vector2.One, callback);
+    public bool Collapse(Action callback = null) => AnimateScale(Vector2.Zero, callback);
+    
+    // VVV DEBUG VVV  //
+
     private bool IsDebug => GetTree().GetCurrentScene() != this;
 
     private void SetupForDebug()
@@ -116,5 +141,8 @@ public partial class Block : Node2D
         if (Input.IsActionJustPressed("ui_left")) direction += Vector2.Left;
         if (Input.IsActionJustPressed("ui_right")) direction += Vector2.Right;
         if (direction != Vector2.Zero) Move(direction * Size);
+
+        if (Input.IsActionJustPressed("ui_accept")) Expand();
+        if (Input.IsActionJustPressed("ui_cancel")) Collapse();
     }
 }
